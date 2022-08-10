1cdcb1f11169f75ab629d6ac80b7f47dc13ac58f
Roman <mazaxaka.tyt@gmail.com>
2022-08-10 18:55:28 +0300

    add react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
+// jsx
 const h1 = <h1>Hello world</h1>
+
+// createElement
+const h2 = React.createElement("h1",null,"Hello world")
 
 function App() {
   return (
-    <div>
-        {h1}
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id consequatur quis esse tenetur magnam unde minima voluptate rem dicta modi commodi iure, recusandae similique eos atque cupiditate omnis quisquam magni.</p>
-    </div>
+    <>
+        <div>
+            <h1>Hello world</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id consequatur quis esse tenetur magnam unde minima voluptate rem dicta modi commodi iure, recusandae similique eos atque cupiditate omnis quisquam magni.</p>
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis incidunt laudantium id! Odit fugit maiores molestiae, pariatur quos soluta quis unde quas nulla reiciendis totam rerum porro, dignissimos officiis delectus!</p>
+        </div>
+    </>
+
   )
 }
 

123ef42666e80284e5b68f49f16dc9b72c5e31e3
Roman <mazaxaka.tyt@gmail.com>
2022-08-10 18:38:10 +0300

    add test text

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
+const h1 = <h1>Hello world</h1>
+
+function App() {
+  return (
+    <div>
+        {h1}
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id consequatur quis esse tenetur magnam unde minima voluptate rem dicta modi commodi iure, recusandae similique eos atque cupiditate omnis quisquam magni.</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("Hello world");
+root.render(<App />);

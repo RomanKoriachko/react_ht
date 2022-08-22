729d8e5b25409480ba05424f942376c6198d7003
Roman <mazaxaka.tyt@gmail.com>
2022-08-22 20:20:20 +0300

    variant 3, arrow function

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -27,11 +27,6 @@ class ProductListItem extends Component<ProductProps, State> {
         count: 1,
     }
 
-    constructor(props: ProductProps) {
-        super(props)
-        this.onDecrementClick = this.onDecrementClick.bind(this)
-    }
-
     onDecrementClick() {
         this.setState((prevState: State) => ({
             count: prevState.count - 1,
@@ -56,7 +51,7 @@ class ProductListItem extends Component<ProductProps, State> {
                     <div className="product-quantity">
                         <Button
                             variant="contained"
-                            onClick={this.onDecrementClick}
+                            onClick={() => this.onDecrementClick()}
                         >
                             -
                         </Button>

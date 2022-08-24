8ae76e28c78e8822d2f5ad88e39c5c435cabf25d
Roman <mazaxaka.tyt@gmail.com>
2022-08-24 19:39:50 +0300

    add change color with hooks

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Button,
     Card,
@@ -26,22 +26,42 @@ const ProductListItem = ({
     price,
     image,
 }: ProductProps) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrementClick = () => {
+        setCount((prevState: number) => prevState + 1)
+    }
+    const onDecrementClick = () => {
+        setCount((prevState: number) => prevState - 1)
+    }
+
+    const [color, setColor] = useState<string>('green')
+    const changeColor = () => {
+        setColor((prevState: string) => (prevState === 'red' ? 'green' : 'red'))
+    }
+
     return (
         <Card>
             <CardContent>
                 <div className="product-img">
                     <img src={image} alt="" />
                 </div>
-                <h4 className="product-title">{name}</h4>
+                <h4 className={`product-title ${color}`}>{name}</h4>
                 <div className="product-description">{description}</div>
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">$ {price}</div>
                 <div className="product-quantity">
-                    <Button variant="contained">-</Button>
-                    <TextField size="small" value={1} variant="outlined" />
-                    <Button variant="contained">+</Button>
+                    <Button variant="contained" onClick={onDecrementClick}>
+                        -
+                    </Button>
+                    <TextField size="small" value={count} variant="outlined" />
+                    <Button variant="contained" onClick={onIncrementClick}>
+                        +
+                    </Button>
                 </div>
+                <p>Color: {color}</p>
+                <button onClick={changeColor}>Change color</button>
             </CardContent>
             <CardActions className="btn-wrap">
                 <Button variant="contained">Add to cart</Button>

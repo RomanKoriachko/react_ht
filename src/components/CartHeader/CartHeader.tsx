045c0aab0d6b79d0c1e9dea27e035ac990922be7
Roman <mazaxaka.tyt@gmail.com>
2022-08-31 19:42:56 +0300

    add correct method to show product name

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { keys } from 'lodash'
-import productsArray from 'components/Products/productsArray'
+import productsArray, {
+    getProductsObject,
+    Product,
+} from 'components/Products/productsArray'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
+    productsObject?: {
+        [id: number]: Product
+    }
 }
 
-const CartHeader = ({ productsInCart }: Props) => {
+const CartHeader = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
         <div>
             {keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productsArray[parseInt(productId) - 1].name}:{' '}
+                    {productsObject[parseInt(productId)].name}:{' '}
                     {productsInCart[parseInt(productId)]}
                 </div>
             ))}
--- src/components/Products/productsArray.ts
+++ src/components/Products/productsArray.ts
@@ -1,4 +1,15 @@
-const productsArray = [
+export type Product = {
+    id: number
+    name: string
+    description: string
+    type: string
+    capacity: number
+    price: number
+    image: string
+}
+
+
+const productsArray: Product[] = [
     {
         id: 1,
         name: 'iPhone X',
@@ -56,3 +67,10 @@ const productsArray = [
 ]
 
 export default productsArray
+
+export const getProductsObject = (array:Product[]) => {
+    return array.reduce((object, product) => ({
+        ...object,
+        [product.id]:product
+    }),{})
+} 

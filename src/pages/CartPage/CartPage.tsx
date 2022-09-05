5484716779f0824f9986fedbf0055336d99c99f5
Roman <mazaxaka.tyt@gmail.com>
2022-09-05 20:03:31 +0300

    add CartProductListItemExtended

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CartTotal from 'components/Cart/CartTotal'
 import CartProductList from 'components/Cart/CartProductList'
+import { Grid } from '@mui/material'
+import CartProductListItemExtended from 'components/Cart/CartProductListItemExtended'
 
 type Props = {
     productsInCart: {
@@ -10,7 +12,13 @@ type Props = {
 
 const CartPage = ({ productsInCart }: Props) => {
     return (
-        <div>
+        <div style={{ padding: '30px 0' }}>
+            <Grid container spacing={4}>
+                <CartProductList
+                    productsInCart={productsInCart}
+                    CartItem={CartProductListItemExtended}
+                />
+            </Grid>
             <CartProductList productsInCart={productsInCart} />
             <CartTotal productsInCart={productsInCart} />
         </div>

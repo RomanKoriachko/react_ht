e6ae578d86e9c85ffde81b84d8601763e73c391e
Roman <mazaxaka.tyt@gmail.com>
2022-09-12 20:37:07 +0300

    add isLiked in CartExtended

--- src/components/Cart/CartProductListItemExtended.tsx
+++ src/components/Cart/CartProductListItemExtended.tsx
@@ -2,6 +2,9 @@ import { Product } from 'components/Products/productsArray'
 import { Card, CardContent, Grid, Button } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Quantity from 'components/Quantity/Quantity'
+import { useAppSelector } from 'redux/hooks'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 
 type Props = {
     productCount: number
@@ -16,6 +19,9 @@ const CartProductListItemExtended = ({
     removeProductFromCart,
     changeProductQuantity,
 }: Props) => {
+    const isLiked = useAppSelector(
+        (state) => state.productsLikeState[product.id]
+    )
     return (
         <Grid item xs={12} sm={6}>
             <Card>
@@ -34,7 +40,15 @@ const CartProductListItemExtended = ({
                             }}
                         />
                     </div>
-
+                    <div>
+                        <Button variant="outlined">
+                            {isLiked ? (
+                                <FavoriteIcon />
+                            ) : (
+                                <FavoriteBorderIcon />
+                            )}
+                        </Button>
+                    </div>
                     <div>{product.name}</div>
                     <p>Price for one item: {product.price} $</p>
                     <p>Count: {productCount}</p>
